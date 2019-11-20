3e7fd66c2f5eb9b5ae735bbdb538ca2079852355
bayu prahasto <bayoe13@gmail.com>
2019-11-20 13:46:14 +0700

    update name project

--- packages/mobile/android/app/src/main/java/com/myprojectname/MainApplication.java
+++ packages/mobile/android/app/src/main/java/com/myprojectname/MainApplication.java
@@ -1,4 +1,4 @@
-package com.myprojectname;
+package com.unitywallet;
 
 import android.app.Application;
 import android.content.Context;
--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -2,7 +2,7 @@ import { AppRegistry } from 'react-native'
 
 import { App } from 'components/src/App'
 
-AppRegistry.registerComponent('myprojectname', () => App)
-AppRegistry.runApplication('myprojectname', {
+AppRegistry.registerComponent('unitywallet', () => App)
+AppRegistry.runApplication('unitywallet', {
   rootTag: document.getElementById('root'),
 })
