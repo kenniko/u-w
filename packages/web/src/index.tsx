3b64f4eb99e0c054c798a169f3708d4ab55e2b21
kenniko <kenniko.okta@gmail.com>
2019-11-27 17:12:25 +0700

    initial CRNA + react-native-web + electron

--- packages/web/src/index.tsx
+++
@@ -1,8 +0,0 @@
-import { AppRegistry } from 'react-native'
-
-import { App } from 'components/src/App'
-
-AppRegistry.registerComponent('unitywallet', () => App)
-AppRegistry.runApplication('unitywallet', {
-  rootTag: document.getElementById('root'),
-})
