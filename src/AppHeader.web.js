96b1ff53147e3f863c049da1c095700f59669ed2
kenniko <kenniko.okta@gmail.com>
2019-12-05 20:22:31 +0700

    update version 0.0.2

--- src/AppHeader.web.js
+++ src/AppHeader.web.js
@@ -15,7 +15,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     width: '100%',
     height: 200,
-    backgroundColor: 'gray',
+    backgroundColor: 'blue',
   },
   text: {
     fontSize: 36,
